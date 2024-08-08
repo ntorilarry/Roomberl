453bf6df52d15a41e3ef081a11e009dd286e3d00
lntori <lntori99@gmail.com>
2024-08-08 16:32:59 +0000

    general bugs

--- src/presentation/main/members/Members.tsx
+++ src/presentation/main/members/Members.tsx
@@ -20,43 +20,6 @@ const Members = () => {
 
   return (
     <div>
-      {/* <div className="px-4 py-6 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl bg-white dark:bg-slate-800">
-        <div className="mx-auto mb-6 lg:max-w-xl sm:text-center">
-          <p className="inline-block px-3 text-lg font-semibold text-gray-900 dark:text-white mb-4 rounded-full ">
-            Matching Users
-          </p>
-        </div>
-        {matchUsers && matchUsers.length > 0 ? (
-          <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
-            {matchUsers.map((item, key) => (
-              <div key={key}>
-                <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
-                  <img
-                    className="absolute object-cover w-full h-full rounded-xl"
-                    src={`https://ui-avatars.com/api/?name=${item.nickname}&background=random`}
-                    alt="Person"
-                  />
-                </div>
-                <div className="flex flex-col sm:text-center">
-                  <p className="text-lg font-bold dark:text-white">
-                    {item.nickname || "NA"}
-                  </p>
-                  <p className=" text-sm text-gray-800 dark:text-white">
-                    {item.courseOfStudy}
-                  </p>
-                  <p className="mb-5 text-sm text-gray-800 dark:text-white">
-                    Match percentage: {item.matchPercentage}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="text-center text-lg text-gray-800 dark:text-white">
-            No matching users found.
-          </div>
-        )}
-      </div> */}
       <section className="">
         <div className="container px-6 mx-auto">
           <h1 className="text-xl font-semibold text-center text-gray-800 capitalize lg:text-2xl dark:text-white">
@@ -72,7 +35,10 @@ const Members = () => {
                 >
                   <img
                     className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300"
-                    src={`https://ui-avatars.com/api/?name=${item.nickname}&background=random`}
+                    src={
+                      item.user?.image ||
+                      `https://ui-avatars.com/api/?name=${item.nickname}&background=random`
+                    }
                     alt=""
                   />
 
